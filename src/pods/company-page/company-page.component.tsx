a6d4298911630c19f5d9ebec58ac9c7002ac64c6
Salino3 <flaviobcn012@gmail.com>
2025-05-03 11:13:35 +0200

    show params

--- src/pods/company-page/company-page.component.tsx
+++ src/pods/company-page/company-page.component.tsx
@@ -1,12 +1,18 @@
+import React from "react";
+import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "./company-page.styles.scss";
 
 export const CompanyPage: React.FC = () => {
   const { t } = useTranslation("main");
 
+  const params = useParams();
+
   return (
     <div className="rootCompanyPage">
       <h1>{t("company_page")}</h1>
+      <span>{params?.name}</span>
+      <span>{params?.id}</span>
     </div>
   );
 };
