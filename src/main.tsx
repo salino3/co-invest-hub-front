375c0d734c0c53a0048ee8913a1f59b3ee3fa97a
Salino3 <flaviobcn012@gmail.com>
2025-05-04 10:13:05 +0200

    commented strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
@@ -6,9 +6,9 @@ import "./i18next/i18n";
 import "./index.scss";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  // <StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  // </StrictMode>
 );
