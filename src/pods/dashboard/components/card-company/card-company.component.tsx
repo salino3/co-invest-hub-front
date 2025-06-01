9bae910800e60b830b19c4d52d44a3f327419b06
Salino3 <flaviobcn012@gmail.com>
2025-06-01 10:40:34 +0200

    add more parameters

--- src/pods/dashboard/components/card-company/card-company.component.tsx
+++ src/pods/dashboard/components/card-company/card-company.component.tsx
@@ -11,7 +11,16 @@ interface Props {
 
 export const CardCompany: React.FC<Props> = (props) => {
   const { company } = props;
-  const { logo, name, id } = company;
+  const {
+    logo,
+    name,
+    id,
+    hashtags,
+    location,
+    sector,
+    investment_min,
+    investment_max,
+  } = company;
   const [zoomPhoto, setZoomPhoto] = useState<boolean>(false);
 
   return (
@@ -20,21 +29,48 @@ export const CardCompany: React.FC<Props> = (props) => {
         to={routesApp?.company(name, String(id))}
         className="containerCardCompany"
       >
-        <div onClick={() => setZoomPhoto(true)} className="boxLogoCompany">
-          <img
-            src={logo || "/assets/icons/group_3.svg"}
-            alt="Logo"
-            onError={(e) => (e.currentTarget.src = "/assets/icons/group_3.svg")}
-          />
+        <div className="boxUp">
+          {/* boxLeftUp */}
+          <div className="boxLeftUp">
+            <div onClick={() => setZoomPhoto(true)} className="boxLogoCompany">
+              <img
+                src={logo || "/assets/icons/group_3.svg"}
+                alt="Logo"
+                onError={(e) =>
+                  (e.currentTarget.src = "/assets/icons/group_3.svg")
+                }
+              />
+            </div>
+            <ZoomImg
+              img={logo || "/assets/icons/group_3.svg"}
+              alt="Logo"
+              download
+              setShow={setZoomPhoto}
+              show={zoomPhoto}
+            />
+            <span className="textName">{name}</span>
+          </div>
+          <hr />
+          {/* boxCenterUp */}
+          <div className="boxCenterUp">
+            <div className="infoLocation">{location}</div>
+            <div className="infoSector">{sector}</div>
+          </div>
+          <hr />
+          {/* boxRightUp */}
+          <div className="boxRightUp">
+            <div className="infoIMin">{investment_min}</div>
+            <div className="infoIMax">{investment_max}</div>
+          </div>
         </div>
-        <ZoomImg
-          img={logo || "/assets/icons/group_3.svg"}
-          alt="Logo"
-          download
-          setShow={setZoomPhoto}
-          show={zoomPhoto}
-        />
-        <span className="textName">{name}</span>
+
+        <div className="boxDown">
+          <div className="hashtagsList">
+            {hashtags &&
+              hashtags?.length > 0 &&
+              hashtags.map((h: string) => <span>#{h}</span>)}
+          </div>
+        </div>
       </Link>
     </div>
   );
