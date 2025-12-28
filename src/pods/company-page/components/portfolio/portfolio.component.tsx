ceaa69ee01cb9641de056e8b423dfd84149a2a0c
Salino3 <flaviobcn012@gmail.com>
2025-12-28 16:46:14 +0200

    show video

--- src/pods/company-page/components/portfolio/portfolio.component.tsx
+++ src/pods/company-page/components/portfolio/portfolio.component.tsx
@@ -34,5 +34,54 @@ export const Portfolio: React.FC<Props> = (props) => {
   } = props;
 
   console.log("formData", formData);
-  return <div className="rootPortfolio">Portfolio</div>;
+  return (
+    <div className="rootPortfolio">
+      <div className="containerContentPortfolio">
+        {formData &&
+          formData.multimedia &&
+          formData.multimedia.length > 0 &&
+          formData.multimedia.map(
+            (item: Record<string, string>, index: number) => {
+              const isYouTube =
+                item.url.includes("youtube.com") ||
+                item.url.includes("youtu.be");
+
+              const isBase64Video =
+                item.url.startsWith("data:video/") ||
+                (item.url.length > 100 && !item.url.startsWith("http"));
+              return (
+                <div key={index} className="cardContentPortfolio">
+                  {item.description}
+                  {isYouTube ? (
+                    <iframe
+                      width="100%"
+                      height="315"
+                      src={
+                        item.url
+                          .replace("youtu.be/", "www.youtube.com/embed/")
+                          .split("?")[0]
+                      }
+                      title={item.description}
+                      frameBorder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowFullScreen
+                    ></iframe>
+                  ) : (
+                    <video
+                      src={
+                        isBase64Video
+                          ? `data:video/mp4;base64,${item.url}`
+                          : item.url
+                      }
+                      controls
+                      width="100%"
+                    />
+                  )}
+                </div>
+              );
+            }
+          )}
+      </div>
+    </div>
+  );
 };
