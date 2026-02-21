b1f5c2c6ee02b65c85bfb39e87f5d9490aa1267b
Salino3 <flaviobcn012@gmail.com>
2026-02-21 19:17:42 +0200

    refactoring routs and fix memory with URL

--- src/router/app-router.tsx
+++ src/router/app-router.tsx
@@ -42,28 +42,7 @@ const routes: PropsRoutes[] = [
     element: <LazyDashboard />,
     visibility: "private",
   },
-  {
-    path: routesApp?.error404,
-    element: <Navigate to={routesApp?.root} />,
-    visibility: "public",
-  },
 ];
-
-function chooseRoutes(visibility: string) {
-  switch (visibility) {
-    case "public":
-      return <PublicRoutes />;
-
-    case "private":
-      return <PrivateRoutes />;
-
-    case "admin":
-      return <AdminRoutes />;
-
-    default:
-      return null;
-  }
-}
 
 // Layout wrapper component
 const LayoutWrapper: React.FC = () => (
@@ -72,20 +51,37 @@ const LayoutWrapper: React.FC = () => (
   </ContainerLayout>
 );
 
+const reducedRoutes: Record<string, JSX.Element[]> = routes.reduce(
+  (acc, route) => {
+    if (!acc[route.visibility]) {
+      acc[route.visibility] = [];
+    }
+    acc[route.visibility].push(
+      <Route key={route.path} path={route.path} element={route.element} />,
+    );
+    return acc;
+  },
+  {} as Record<string, JSX.Element[]>,
+);
+
 export const AppRoutes: React.FC = () => {
   return (
     <Routes>
       {/* <Suspense fallback={...}> ... */}
-      <Route path={routesApp?.root} element={<LayoutWrapper />}>
-        {routes &&
-          routes?.length > 0 &&
-          routes.map(({ path, element, visibility }) => {
-            return (
-              <Route key={path} path={path} element={chooseRoutes(visibility)}>
-                <Route path={path} element={element} />
-              </Route>
-            );
-          })}
+      <Route element={<LayoutWrapper />}>
+        {/* Grupo Privado */}
+        <Route element={<PrivateRoutes />}>{reducedRoutes["private"]}</Route>
+
+        {/* Grupo Público */}
+        <Route element={<PublicRoutes />}>{reducedRoutes["public"]}</Route>
+
+        {/* Grupo Admin */}
+        <Route element={<AdminRoutes />}>{reducedRoutes["admin"]}</Route>
+
+        <Route
+          path={routesApp?.error404}
+          element={<Navigate to={routesApp.root} />}
+        />
       </Route>
     </Routes>
   );
